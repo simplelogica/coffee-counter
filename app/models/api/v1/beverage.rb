34b16f6bcf931f097ee33eacd1df6b1b9d01e42f
Ruben Sierra <maguilag@gmail.com>
2014-08-22 19:36:39 +0200

    Agregamos validación de presencia y rango para kind

--- app/models/api/v1/beverage.rb
+++ app/models/api/v1/beverage.rb
@@ -1,6 +1,9 @@
 module Api
   module V1
     class Beverage < ActiveRecord::Base
+      extend Enumerize
+      enumerize :kind , in: [:coffee, :tea]
+      validates :kind, presence: true
     end
   end
 end

a1d8ba9e10e243c27d6a63ad65b3351f31631498
Ruben Sierra <maguilag@gmail.com>
2014-08-22 19:25:45 +0200

    Rutas para trabajar con tipos de bebidas.

--- app/controllers/api/v1/beverages_controller.rb
+++ app/controllers/api/v1/beverages_controller.rb
@@ -4,38 +4,19 @@ module Api
       # GET /beverages
       # GET /beverages.json
       def index
-        @beverages = Beverage.all
+        @beverages = Api::V1::Beverage.all
+        @beverages = @beverages.where(kind: params[:kind]) if params[:kind]
 
         render json: @beverages
-      end
-
-      # GET /beverages/1
-      # GET /beverages/1.json
-      def show
-        @beverage = Beverage.find(params[:id])
-
-        render json: @beverage
       end
 
       # POST /beverages
       # POST /beverages.json
       def create
-        @beverage = Beverage.new(params[:beverage])
+        @beverage = Api::V1::Beverage.new(kind: params[:kind])
 
         if @beverage.save
-          render json: @beverage, status: :created, location: @beverage
-        else
-          render json: @beverage.errors, status: :unprocessable_entity
-        end
-      end
-
-      # PATCH/PUT /beverages/1
-      # PATCH/PUT /beverages/1.json
-      def update
-        @beverage = Beverage.find(params[:id])
-
-        if @beverage.update(params[:beverage])
-          head :no_content
+          render json: @beverage, status: :created
         else
           render json: @beverage.errors, status: :unprocessable_entity
         end
@@ -44,7 +25,7 @@ module Api
       # DELETE /beverages/1
       # DELETE /beverages/1.json
       def destroy
-        @beverage = Beverage.find(params[:id])
+        @beverage = Api::V1::Beverage.where(kind: params[:kind]).last
         @beverage.destroy
 
         head :no_content
